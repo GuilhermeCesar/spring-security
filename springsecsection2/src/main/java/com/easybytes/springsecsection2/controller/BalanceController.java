c66522457058df3d4fe2079681c9bb6f51fd4374
guilhermecesar <guilherme.medeiros@platformbuilders.io>
2025-07-05 17:43:43 -0300

    Create base project to phase 2

---
+++ springsecsection2/src/main/java/com/easybytes/springsecsection2/controller/BalanceController.java
@@ -0,0 +1,15 @@
+package com.easybytes.springsecsection2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+
+
+
+    @GetMapping("/myBalance")
+    public String getbalanceDetails() {
+        return "Here are the balance details from the DB";
+    }
+}
---
+++ springsecsection2/src/main/java/com/easybytes/springsecsection2/controller/NoticesController.java
@@ -0,0 +1,15 @@
+package com.easybytes.springsecsection2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+
+
+
+    @GetMapping("/notices")
+    public String getNotices() {
+        return "Here are the notices from the DB";
+    }
+}
