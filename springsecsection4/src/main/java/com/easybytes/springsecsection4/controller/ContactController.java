45cf6bca21cd82f907339b778330573f85139fb9
guilhermecesar <guilherme.medeiros@platformbuilders.io>
2026-03-19 17:49:07 -0300

    Create a JDBC integration from spring default tables

---
+++ springsecsection4/src/main/java/com/easybytes/springsecsection4/controller/ContactController.java
@@ -0,0 +1,15 @@
+package com.easybytes.springsecsection4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+
+
+    @GetMapping("/contact")
+    public String sayWelcome() {
+        return "Inquiry defail are saved to the DB";
+    }
+}
